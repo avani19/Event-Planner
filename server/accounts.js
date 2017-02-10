a7bf9b88f0da8538ca98685bdae546bc5e2fbe7b
avani19 <avaniipatels5@gmail.com>
2017-02-10 18:52:08 -0500

    Set github authentication

--- server/accounts.js
+++ server/accounts.js
@@ -3,18 +3,11 @@ ServiceConfiguration.configurations.remove({
   service: "github"
 });
 
-// chicago-parties.meteor.com
-// ServiceConfiguration.configurations.insert({
-//   service: "github",
-//   clientId: "5c9b7e93a5830dd3634a",
-//   secret: "1fd43cfe0e0311e726e239a7a1bec03f8c3c2c89"
-// });
-
 // localhost:3000
 ServiceConfiguration.configurations.insert({
   service: "github",
-  clientId: "83f1c796a14e1f6ea61a",
-  secret: "1c59890a41679554964713050e237ee89332731b"
+  clientId: "26fe7471b2ea84b9129a",
+  secret: "ae2691cf1119729068ff5548758ab929a46e8157"
 });
 
 Accounts.onCreateUser(function(options, user){
